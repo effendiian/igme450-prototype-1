e4e99e8f50258e94bfccd481ea11a9354c7625a1
MITCHELL-COMPUT\Mitchell <mwh2719@rit.edu>
2020-01-19 14:25:52 -0500

    Like Button
    Starting work on like button prototype

---
+++ GAME01_IGME450/Assets/Scripts/Hmm behavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hmmbehavior : MonoBehaviour
+{
+
+    private GameObject audioClip;   //Reference to the game objecct holding the hmm audio clip
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
