30144a25ff70ee3afa86c4dbc06292a93ce927fa
MITCHELL-COMPUT\Mitchell <mwh2719@rit.edu>
2020-01-23 17:08:54 -0500

    Button Updates
    Adding dislike button with sound effect. Styling both buttons to be easier to understand. Adding popularity counter and functionality to run it.

---
+++ GAME01_IGME450/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehavior : MonoBehaviour
+{
+
+    public GameObject popularityCounterObject;  //UI object that counnts the painting popularity
+    private Text popularityCounter;     //Reference to the text on the UI object for counting the painting popularity
+    private int popularityScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        popularityCounter = popularityCounterObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        popularityCounter.text = "Popularity: " + popularityScore;
+    }
+
+
+    //function that runs when a like happens
+    public void Like()
+    {
+        popularityScore++;
+    }
+
+    //function that runs when a dislike happens
+    public void Dislike()
+    {
+        if (popularityScore > 0)
+        {
+            popularityScore--;
+        }
+    }
+}
