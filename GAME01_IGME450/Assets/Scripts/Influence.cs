f2315b6fadaa1af80c138286376e3f84732935dc
MITCHELL-COMPUT\Mitchell <mwh2719@rit.edu>
2020-01-30 13:06:46 -0500

    Implmenting Influence
    Implmenting the influence bar that fills up with time and resets when the painting is liked. It also runs a multiplier

---
+++ GAME01_IGME450/Assets/Scripts/Influence.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Influence : MonoBehaviour
+{
+
+    //varaibles
+    private float multiplier; //float to hold the current multiplier value
+    private float reputationScoreMultiplier;    //float to decide how fast the bar will fill and multiplier will increase
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        reputationScoreMultiplier = .02f;
+        multiplier = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //fixed update to run the timer for the influence bar and the mulitplier
+    private void FixedUpdate()
+    {
+        multiplier += .1f;
+
+        this.transform.localScale = new Vector3(this.transform.localScale.x+reputationScoreMultiplier, this.transform.localScale.y);
+    }
+
+    //function to return the current multiplier that will effect popularity
+    public float Mulitplier
+    {
+        get { return multiplier; }
+    }
+
+    //function to reset the influence when it is used
+    public void ResetInfluence()
+    {
+        this.transform.localScale = new Vector3(.03f, this.transform.localScale.y);
+    }
+}
