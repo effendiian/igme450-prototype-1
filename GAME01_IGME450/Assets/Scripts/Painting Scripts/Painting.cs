6dadff969031562cc19e286cd86217849c86501d
Steven Domitrz <stevendomitrz@gmail.com>
2020-02-03 14:28:00 -0500

    Set up painting to only base popularity off multiplier given

--- GAME01_IGME450/Assets/Scripts/Painting Scripts/Painting.cs
+++ GAME01_IGME450/Assets/Scripts/Painting Scripts/Painting.cs
@@ -7,14 +7,8 @@ public class Painting : MonoBehaviour
     private ColorTrait color;
     private FormatTrait format;
 
-
-    private int timer = 0;      //Timer for keeping track of how often the like button is being used
-    private int frames = 60;    //Frame count used for incrmenting the timer
-    private int successfulClicks;
-    private int badClicks;
-    private bool tooManyLikes;   //Bool for holding whether or not the player has liked or disliked the painting too often
-
-    public static int SECONDS_BETWEEN_CLICKS = 2;
+    private int BASE_CLICK_INCREASE = 2;
+    private int clicks;
 
     //Keep track of initial and end popularity over the night
     private int initialPopularity;
@@ -25,20 +19,6 @@ public class Painting : MonoBehaviour
     //{
         
     //}
-
-    private void FixedUpdate()
-    {
-        if (frames <= 0)
-        {
-            timer++;
-            Debug.Log(timer);
-            frames = 60;
-        }
-        else
-        {
-            frames--;
-        }
-    }
 
     public void Upvote(float multiplier)
     {
@@ -58,86 +38,25 @@ public class Painting : MonoBehaviour
 
     private void HandleClick(float multiplier, int sign)
     {
-        if (SuccessfulClick())
+        clicks += 1;
+
+        if (multiplier > 0)
         {
-            int change = Mathf.FloorToInt(sign * multiplier * GetSuccessfullClickFactor());
-            if (change != 0)
+            popularity += (int)(multiplier * BASE_CLICK_INCREASE * sign);
+        }
+        else
+        {
+            //Click which has a negative impact, don't go lower than the start
+            if ((sign > 0 && GetChangeInPopularity() >= 0) || GetChangeInPopularity() <= 0)
             {
-                successfulClicks++;
-                popularity += Mathf.FloorToInt(sign * multiplier * GetSuccessfullClickFactor());
+                popularity -= (int)(multiplier * BASE_CLICK_INCREASE * sign);
             }
         }
-        else
-        {
-            DoBadClick(-sign);
-        }
     }
 
-    private float GetSuccessfullClickFactor()
+    public int GetNumberOfClicks()
     {
-        if (successfulClicks < 2)
-        {
-            return 1;
-        }
-        else if (successfulClicks < 5)
-        {
-            return 0.8f - (0.2f * (successfulClicks - 2));
-        }
-        else if (successfulClicks < 12)
-        {
-            return 0.4f;
-        }
-        else if (successfulClicks < 18) //Was 7 and 0.1, jumped straight from previous
-        {
-            return 0.2f;
-        }
-        else
-        {
-            return 0.025f;
-        }
-    }
-
-    private void DoBadClick(int sign)
-    {
-        badClicks++;
-
-        //If this is the first bad click or there has been no net change in popularity don't do anything
-        if (badClicks == 1 || GetChangeInPopularity() <= 0)
-        {
-            return;
-        }
-        
-        if (badClicks <= 3)
-        {
-            popularity += 1 * sign;
-        }
-        else if (badClicks <= 5)
-        {
-            popularity += 3 * sign;
-        }
-        else if (badClicks <= 8)
-        {
-            popularity += 6 * sign;
-        }
-    }
-
-    private bool SuccessfulClick()
-    {
-        if (successfulClicks == 0)
-        {
-            return true;
-        }
-        else
-        {
-            if (timer > SECONDS_BETWEEN_CLICKS)
-            {
-                timer = 0;
-                return true;
-            } else
-            {
-                return false;
-            }
-        }
+        return clicks;
     }
 
     //Accessible point for getting the popularity to display on the screen
@@ -150,18 +69,6 @@ public class Painting : MonoBehaviour
     {
         return popularity - initialPopularity;
     }
-
-    // Start is called before the first frame update
-    //void Start()
-    //{
-        
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
 
     public void SetTraits(ColorTrait color, FormatTrait format)
     {
