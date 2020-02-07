c58d1cd26fd6426ecd55d69b1f5be9350c6e9af0
twb2511 <twb2511@rit.edu>
2020-02-07 01:04:32 -0500

    rip

---
+++ GAME01_IGME450/Assets/Scripts/GolfMeter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfMeter : MonoBehaviour
+{
+    public GameObject meterTicker;
+    public Vector3 leftEdge;
+    public Vector3 rightEdge;
+    public float lerpPercent = 0.0f;
+    public int count = 1;
+    public int speed = 1;
+    public bool goingRight = true;
+    GameObject ticker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftEdge = new Vector3(transform.position.x - 2.5f, transform.position.y, transform.position.z);
+        rightEdge = new Vector3(transform.position.x + 2.5f, transform.position.y, transform.position.z);
+
+        ticker = Object.Instantiate(meterTicker);
+        ticker.transform.position = leftEdge;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(goingRight)
+        {
+            lerpPercent += 0.005f * speed;
+
+            if(lerpPercent >= 1.0f)
+            {
+                goingRight = false;
+            }
+        }
+        else
+        {
+            lerpPercent -= 0.005f * speed;
+
+            if(lerpPercent <= 0.0f)
+            {
+                goingRight = true;
+            }
+        }
+
+        ticker.transform.position = Vector3.Lerp(leftEdge, rightEdge, lerpPercent);
+
+    }
+
+    public int GetBonus()
+    {
+        count++;
+
+        speed *= count;
+
+        if (speed >= 3) { speed = 3; }
+
+        int multiplier = 1;
+
+        if(lerpPercent >= 0.45f && lerpPercent <= 0.55f)
+        {
+            multiplier = 2;
+        }
+        else if (lerpPercent >= 0.48f && lerpPercent <= 0.52f)
+        {
+            multiplier = 4;
+        }
+
+        Debug.Log(multiplier);
+        return multiplier;
+    }
+}
